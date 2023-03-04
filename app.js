585a376a8c3b5e73c89e154e7353095a33e9dd23
Vadim Sokolovsky <vadimirsvs@gmail.com>
2023-03-04 10:51:49 +0300

    Update

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const _ = require("lodash");
+const serverless = require("serverless-http");
 
 const app = express();
+const router = express.Router();
 
 app.set('view engine', 'ejs');
 
@@ -146,6 +148,8 @@ app.post("/delete", function (req, res) {
 app.get("/about", function (req, res) {
   res.render("about");
 });
+
+app.use("./netlify/functions/api", router);
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
